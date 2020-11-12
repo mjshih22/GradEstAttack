ea51196ff89facdcf4d6ca96e94f710de27e7498
mjshih22 <58533059+mjshih22@users.noreply.github.com>
2020-11-11 23:23:54 -0500

    new grad code

--- AlexNet/AlexNet100Grad.py
+++ AlexNet/AlexNet100Grad.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
-
 import torch
 import torchvision
 import numpy as np
+import torch.optim as optim
 import torchvision.transforms as transforms
+import torch.nn as nn
+from alexnet import alexnet
+from getgrad import getgrad
 
+# import CIFAR100 data
 transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
@@ -15,52 +18,33 @@ transform = transforms.Compose([
 
 trainset = torchvision.datasets.CIFAR100(root='./data', train=True,
                                         download=True, transform=transform)
-
 testset = torchvision.datasets.CIFAR100(root='./data', train=False,
                                        download=True, transform=transform)
 
-att_train_train, att_test_train, _ = torch.utils.data.random_split(trainset, [8000,2000, 40000], generator=torch.Generator().manual_seed(42))
-att_train_test, att_test_test = torch.utils.data.random_split(testset, [8000,2000], generator=torch.Generator().manual_seed(42))
+# convert data into shadow and target dataloaders. use manual seed to fix dataloaders between instances
+train_size = 8000
+val_size = 1000
+test_size = 1000
+data_train_size = 50000
+remain_size = data_train_size-train_size-val_size-test_size
 
-att_train_train_loader = torch.utils.data.DataLoader(att_train_train, batch_size=100, shuffle=False, num_workers=2)
-att_train_test_loader = torch.utils.data.DataLoader(att_train_test, batch_size=100, shuffle=False, num_workers=2)
-att_test_train_loader =  torch.utils.data.DataLoader(att_test_train, batch_size=100, shuffle=False, num_workers=2)
-att_test_test_loader = torch.utils.data.DataLoader(att_test_test, batch_size=100, shuffle=False, num_workers=2)
+torch.manual_seed(42)
+att_train_train, att_val_train, att_test_train, _ = torch.utils.data.random_split(trainset, [train_size, val_size, test_size, remain_size])
+torch.manual_seed(torch.initial_seed())
+att_train_test, att_val_test, att_test_test = torch.utils.data.random_split(testset, [train_size, val_size, test_size])
+torch.manual_seed(torch.initial_seed())
 
-import torch.nn as nn
+att_train_train_loader = torch.utils.data.DataLoader(att_train_train, batch_size=256, shuffle=False, num_workers=2)
+att_train_test_loader = torch.utils.data.DataLoader(att_train_test, batch_size=256, shuffle=False, num_workers=2)
+att_val_train_loader = torch.utils.data.DataLoader(att_val_train, batch_size=256, shuffle=False, num_workers=2)
+att_val_test_loader = torch.utils.data.DataLoader(att_val_test, batch_size=256, shuffle=False, num_workers=2)
+att_test_train_loader =  torch.utils.data.DataLoader(att_test_train, batch_size=256, shuffle=False, num_workers=2)
+att_test_test_loader = torch.utils.data.DataLoader(att_test_test, batch_size=256, shuffle=False, num_workers=2)
 
-class AlexNet(nn.Module):
-
-    def __init__(self, num_classes=100):
-        super(AlexNet, self).__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=5),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(64, 192, kernel_size=5, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-        )
-        self.classifier = nn.Linear(256, num_classes)
-
-    def forward(self, x):
-        x = self.features(x)
-        x = x.view(x.size(0), -1)
-        x = self.classifier(x)
-        return x
-    
-alexnet = AlexNet()
-
+# create AlexNet model with pretrained parameters
+target = alexnet(num_classes = 100)
 
 PATH = './model_best.pth.tar'
-
 checkpoint = torch.load(PATH)
 state_dict = checkpoint['state_dict']
 
@@ -73,126 +57,31 @@ for k, v in state_dict.items():
     else:
         k = k.replace('module.', '')
     new_state_dict[k] = v
+target.load_state_dict(new_state_dict)
 
-alexnet.load_state_dict(new_state_dict)
+target.eval()
 
-alexnet.eval()
-
-# set criterion
+# set criterion for loss
 criterion = nn.CrossEntropyLoss()
 
-# get grad on train data
-for i, data in enumerate(att_train_train_loader):
-    resnet.zero_grad()
-    input, target = data
-    input.requires_grad_(True)
-    output = resnet(input.cuda())
-
-    loss = criterion(output, target.cuda())
-    loss.backward()
-    g = input.grad.data
-    
-    if i == 0:
-        att_train_input = g
-    else:
-        att_train_input = torch.cat((att_train_input, g))
-        
-for i, data in enumerate(att_train_test_loader):
-    resnet.zero_grad()
-    input, target = data
-    input.requires_grad_(True)
-    output = resnet(input.cuda())
-
-    loss = criterion(output, target.cuda())
-    loss.backward()
-    g = input.grad.data
-    att_train_input = torch.cat((att_train_input, g))
-
-# make targets 
-att_train_label = torch.ones(16000, dtype = torch.int64)
-for i in range(0,8000):
-    att_train_label[i] = 0
-
-# create data loader
-att_train_dataset = torch.utils.data.TensorDataset(att_train_input, att_train_label)
-att_train_dataloader = torch.utils.data.DataLoader(att_train_dataset, batch_size=256,
-                                          shuffle=True, num_workers=2)
-
-# get grad on test data
-for i, data in enumerate(att_test_train_loader):
-    resnet.zero_grad()
-    input, target = data
-    input.requires_grad_(True)
-    output = resnet(input.cuda())
-
-    loss = criterion(output, target.cuda())
-    loss.backward()
-    g = input.grad.data
-    
-    if i == 0:
-        att_test_input = g
-    else:
-        att_test_input = torch.cat((att_test_input, g))
-        
-for i, data in enumerate(att_test_test_loader):
-    resnet.zero_grad()
-    input, target = data
-    input.requires_grad_(True)
-    output = resnet(input.cuda())
-
-    loss = criterion(output, target.cuda())
-    loss.backward()
-    g = input.grad.data
-    att_test_input = torch.cat((att_test_input, g))
-
-# make targets 
-att_test_label = torch.ones(4000, dtype = torch.int64)
-for i in range(0,2000):
-    att_test_label[i] = 0
-
-# create data loader
-att_test_dataset = torch.utils.data.TensorDataset(att_test_input, att_test_label)
-att_test_dataloader = torch.utils.data.DataLoader(att_test_dataset, batch_size=500,
-                                          shuffle=False, num_workers=2)
-
+# create data loader with gradients for attack
+att_train_dataloader = getgrad(target, criterion, att_train_train_loader, att_train_test_loader, train_size, batch_size=512)
+att_val_dataloader = getgrad(target, criterion, att_val_train_loader, att_val_test_loader, val_size)
+att_test_dataloader = getgrad(target, criterion, att_test_train_loader, att_test_test_loader, test_size)
 print("Data has been loaded")
-# create attack model
-class Attack(nn.Module):
-
-    def __init__(self, num_classes=2):
-        super(Attack, self).__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=5),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(64, 192, kernel_size=5, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-        )
-        self.classifier = nn.Linear(256, num_classes)
-
-    def forward(self, x):
-        x = self.features(x)
-        x = x.view(x.size(0), -1)
-        x = self.classifier(x)
-        return x
     
-attack = Attack()
+# create attack model
+attack = alexnet(num_classes = 2)
+attack = nn.DataParallel(attack).cuda()
 
 # set optimizer
-import torch.optim as optim
-
 optimizer = optim.Adam(attack.parameters(), lr=0.001, betas=(0.9,0.999),eps=1e-08,weight_decay=0,amsgrad=False)
 
+best_acc = 0
+PATH = './attack_net.pth'
+
 # train attack model
-for epoch in range(10):  # loop over the dataset multiple times
+for epoch in range(50):  # loop over the dataset multiple times
     running_loss = 0.0
     for i, data in enumerate(att_train_dataloader, 0):
         # get the inputs; data is a list of [inputs, labels]
@@ -202,16 +91,16 @@ for epoch in range(10):  # loop over the dataset multiple times
         optimizer.zero_grad()
 
         # forward + backward + optimize
-        outputs = attack(inputs)
-        loss = criterion(outputs, labels)
+        outputs = attack(inputs.cuda())
+        loss = criterion(outputs, labels.cuda())
         loss.backward()
         optimizer.step()
 
         # print statistics
         running_loss += loss.item()
-        if i % 60 == 59:    # print once per epoch
+        if i % 24 == 23:    # print once per epoch
             print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 60))
+                  (epoch + 1, i + 1, running_loss / 24))
             running_loss = 0.0
 
 	    # evaluate attack mode
@@ -222,31 +111,62 @@ for epoch in range(10):  # loop over the dataset multiple times
             with torch.no_grad():
                 for data in att_test_dataloader:
                     images, labels = data
-                    outputs = attack(images)
+                    outputs = attack(images.cuda())
                     _, predicted = torch.max(outputs.data, 1)
                     total += labels.size(0)
-                    correct += (predicted == labels).sum().item()
+                    correct += (predicted == labels.cuda()).sum().item()
             print('Accuracy of the network on the test images using gradients of inputs: {acc:.3f}'.format(acc=100*correct/total))
-
+            acc=100*correct/total
+            if acc > best_acc:
+                print(best_acc)
+                best_acc = acc
+                torch.save(attack.state_dict(), PATH)
             attack.train()
-
 
 print('Finished Training')
 
-PATH = './attack_net.pth'
-torch.save(attack.state_dict(), PATH)
+# load best model
+attack.load_state_dict(torch.load(PATH))
 
-# evaluate attack mode
+# evaluate attack model on train and test data
 attack.eval()
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in att_train_dataloader:
+        images, labels = data
+        outputs = attack(images.cuda())
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels.cuda()).sum().item()
+
+print('Accuracy of the network on the train images using gradients of inputs: {acc:.3f}'.format(acc=100*correct/total))
 
 correct = 0
 total = 0
 with torch.no_grad():
     for data in att_test_dataloader:
         images, labels = data
+        outputs = attack(images.cuda())
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels.cuda()).sum().item()
+
+print('Accuracy of the network on the test images using gradients of inputs: {acc:.3f}'.format(acc=100*correct/total))
+
+
+""" # evaluate attack model on train and test data
+attack.eval()
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in att_val_dataloader:
+        images, labels = data
         outputs = attack(images)
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
 
-print('Accuracy of the network on the test images using gradients of inputs: {acc:.3f}'.format(acc=100*correct/total))
+print('Accuracy of the network on the validation images using gradients of inputs: {acc:.3f}'.format(acc=100*correct/total)) """
---
+++ AlexNet/getgrad.py
@@ -0,0 +1,43 @@
+import torch
+import torchvision
+import numpy as np
+
+def getgrad(model, criterion, train_loader, test_loader, input_size, batch_size = 256):
+
+    # get grad on train data
+    for i, data in enumerate(train_loader):
+        model.zero_grad()
+        input, target = data
+        input.requires_grad_(True)
+        output = model(input)
+
+        loss = criterion(output, target)
+        loss.backward()
+        g = input.grad.data
+        
+        if i == 0:
+            att_input = g
+        else:
+            att_input = torch.cat((att_input, g))
+
+    for i, data in enumerate(test_loader):
+        model.zero_grad()
+        input, target = data
+        input.requires_grad_(True)
+        output = model(input)
+        loss = criterion(output, target)
+        loss.backward()
+        g = input.grad.data
+        att_input = torch.cat((att_input, g))
+
+    # make targets 
+    att_label = torch.ones(2*input_size, dtype = torch.int64)
+    for i in range(0,input_size):
+        att_label[i] = 0
+
+    # create data loader
+    att_dataset = torch.utils.data.TensorDataset(att_input, att_label)
+    att_dataloader = torch.utils.data.DataLoader(att_dataset, batch_size=batch_size,
+                                            shuffle=True, num_workers=2)
+
+    return att_dataloader
